9019acb0b3468d33309e28bab9b4b5a1bf2cca9f
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-03-06 13:38:01 +0000

    zen of python

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3Zen of Python, by Tim Peters
+#!/usr/bin/python3
 str = f"""
 The Zen of Python, by Tim Peters
 
@@ -20,6 +20,5 @@ Now is better than never.
 Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!
-"""
+Namespaces are one honking great idea -- let's do more of those!"""
 print(str)
