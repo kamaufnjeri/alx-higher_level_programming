c8dba04f82ae726ed5209c3eba5a0da7f0ed81c2
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-03-07 12:55:07 +0000

    print nums

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for num1 in range(0, 10):
-    for num2 in range(num1 + 1, 10):
-        print("{}{}".format(num1, num2), end=", ")
     if num1 == 8:
         print(89)
+    else:
+        for num2 in range(num1 + 1, 10):
+            print("{}{}".format(num1, num2), end=", ")
+
