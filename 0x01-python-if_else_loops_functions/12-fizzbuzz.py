e1ff40a664cdcbec0ed45be2e008b37b4b57d712
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-03-07 14:22:30 +0000

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 for num in range(1, 101):
-    if num == 100:
-        print("Buzz")
     elif (num % 5 == 0) and (num % 3 == 0):
         print("FizzBuzz", end=" ")
     elif (num % 5 == 0):
