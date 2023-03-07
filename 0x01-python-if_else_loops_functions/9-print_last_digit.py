fd1e447f622257eb11e24df6b8d4fe849b5b9db3
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-03-07 13:40:52 +0000

    last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number >= 0:
+        last = number % 10
+    else:
+        number *= -1
+        last = number % 10
+    print(last, end="")
+    return(last)
