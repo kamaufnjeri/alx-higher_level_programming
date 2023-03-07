cc0292b336701d36022f4042c83f1ea6762030e1
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-03-07 18:35:22 +0300

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-for num in range(1, 100):
-    if (num % 5 == 0) and (num % 3 == 0):
+for num in range(1, 101):
+    if num == 100:
+        print("Buzz ")
+    elif (num % 5 == 0) and (num % 3 == 0):
         print("FizzBuzz", end=" ")
     elif (num % 5 == 0):
         print("Buzz", end=" ")
     elif (num % 3 == 0):
         print("Fizz", end=" ")
     else:
-        print(f"{num}", end=" ")
-print("Buzz ")
+        print(f"{num}", end=" "
