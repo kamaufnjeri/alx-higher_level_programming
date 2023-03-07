7487140679288cc47e09ac89ba26457cc833e0e4
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-03-07 12:15:59 +0000

    0 - 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for a in range(0, 99):
-    print("{:d}".format(a), end=", ")
+    if a < 10:
+        print("0{}".format(a), end=", ")
+    else:
+        print("{:d}".format(a), end=", ")
 print(99)
