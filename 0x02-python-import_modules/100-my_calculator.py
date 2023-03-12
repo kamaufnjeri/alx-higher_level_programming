603ccbf8478e0ac61a7f9e5428cb258d1d59bc78
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-03-12 15:05:22 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
-from sys import argv, exit
-from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    if (len(argv) != 4):
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    a = sys.argv[1]
+    b = sys.argv[3]
+    operators = ['+', '-', '*', '/']
+    if (len(sys.argv) - 1) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    operator = argv[2]
-    a = int(argv[1])
-    b = int(argv[3])
-    if (operator == "+"):
-        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    elif (operator == "-"):
-        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    elif (operator == '*'):
-        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    elif (operator == "/"):
-        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+        exit (1)
     else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if sys.argv[2] == '+':
+            print(f"{sys.argv[1]} + {sys.argv[3]} = {add(a, b)}")
+        elif sys.argv[2] == '-':
+            print(f"{sys.argv[1]} - {sys.argv[3]} = {sub(a, b)}")
+        elif sys.argv[2] == '*':
+            print(f"{sys.argv[1]} * {sys.argv[3]} = {mul(a, b)}")
+        elif sys.argv[2] == '/':
+            print(f"{sys.argv[1]} / {sys.argv[3]} = {div(a, b)}")
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit (1)
