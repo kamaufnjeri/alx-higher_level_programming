e8e6a20e4b3ca2a7b8b7efb41d204509f24d7bce
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-03-12 15:07:43 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,9 +4,6 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
 
-    a = sys.argv[1]
-    b = sys.argv[3]
-    operators = ['+', '-', '*', '/']
     if (len(sys.argv) - 1) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit (1)
