30b25a44d3700f4539e55e28ca5b8b17d5ad0afa
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-03-13 17:41:47 +0300

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,90 +1,72 @@
 #include "lists.h"
 
-/**
- * length_linked_list - determines the length of a linked list.
- * @head: pointer to the head of a linked list.
- *
- * Return: length of a linked list.
- */
-int length_linked_list(listint_t *head)
-{
-	int length = 0;
+listint_t *reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
 
-	for (; head; length++)
+/**
+ * reverse_listint - Reverses a singly-linked listint_t list.
+ * @head: A pointer to the starting node of the list to reverse.
+ *
+ * Return: A pointer to the head of the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
+
+	while (node)
 	{
-		head = head->next;
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
 	}
 
-	return (length);
+	*head = prev;
+	return (*head);
 }
 
 /**
- * linked_to_array - creates an array with the values of the linked list.
- * @head: pointer to the head of the linked list.
- * @size:  size of the linked list.
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: A pointer to the head of the linked list.
  *
- * Return: Pointer to array with values of linked list.
- */
-int *linked_to_array(listint_t *head, int size)
-{
-	int *array_of_linked, *copy_array, i;
-
-	if (head == NULL)
-	{
-		return (NULL);
-	}
-	array_of_linked = malloc(sizeof(int) * size);
-	if (array_of_linked == NULL)
-	{
-		return (NULL);
-	}
-	copy_array = array_of_linked;
-	for (i = 0; i < size; i++)
-	{
-		*copy_array = head->n;
-		copy_array++;
-	}
-	return (array_of_linked);
-}
-
-/**
- * is_palindrome - checks if a linked list is a palindrome.
- * @head: pointer to the head of the linked list.
- *
- * Return: 0 if is not palindrome, 1 instead.
+ * Return: If the linked list is not a palindrome - 0.
+ *         If the linked list is a palindrome - 1.
  */
 int is_palindrome(listint_t **head)
 {
-	int length = 0, *array_numbers, i, j;
+	listint_t *tmp, *rev, *mid;
+	size_t size = 0, i;
 
-	if (head == NULL || *head == NULL)
-	{
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	}
-	length = length_linked_list(*head);
-	if (length == 0)
+
+	tmp = *head;
+	while (tmp)
 	{
-		return (1);
+		size++;
+		tmp = tmp->next;
 	}
-	array_numbers = linked_to_array(*head, length);
-	if (array_numbers == NULL)
-	{
+
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmp = tmp->next;
+
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
 		return (0);
-	}
-	j = length - 1;
-	for (i = 0; i < (length / 2); i++)
+
+	tmp = tmp->next->next;
+	rev = reverse_listint(&tmp);
+	mid = rev;
+
+	tmp = *head;
+	while (rev)
 	{
-		if (array_numbers[i] == array_numbers[j])
-		{
-			continue;
-		}
-		else
-		{
-			free(array_numbers);
+		if (tmp->n != rev->n)
 			return (0);
-		}
-		j--;
+		tmp = tmp->next;
+		rev = rev->next;
 	}
-	free(array_numbers);
+	reverse_listint(&mid);
+
 	return (1);
 }
