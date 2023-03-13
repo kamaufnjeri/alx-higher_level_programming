e27b69e0272468b98f0edc8e887284152388d636
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-03-13 12:47:13 +0000

    add tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -15,5 +15,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     integer_at_index0 = tuple_a[0] + tuple_b[0]
     integer_at_index1 = tuple_a[1] + tuple_b[1]
 
-    tuple_sum = integer_at_index0, integer_at_index1
-    return tuple_sum
+    return (integer_at_index0, integer_at_index1)
