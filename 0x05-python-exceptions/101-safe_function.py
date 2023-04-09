34f40e077cc23ebc9ae7bdbf599ab4a0ddb35a36
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-04-09 15:09:28 +0300

    Update 101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         div = fct(*args)
         return div
-    except (DivisionZeroError, IndexError) as msg:
+    except (ZeroDivisionError, IndexError) as msg:
         print("Exception: {}".format(msg), file=sys.stderr)
         return None
