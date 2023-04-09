32b06ab1c8a0a0a2f0cf9f125d934ce96a93d9ea
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-04-09 14:26:45 +0300

    Update 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,6 +3,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except TypeError as msg:
+    except (ValueError, TypeError) as msg:
         sys.stderr.write("{}".format(msg))
         return False
