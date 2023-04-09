2cb1ee13975ff0c915cfabb5a69ec532eae1ff36
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-04-09 15:02:26 +0300

    Create 101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/use/bin/python3
+
+import sys
+
+def safe_function(fct, *args):
+    try:
+        div = fct(*args)
+        return div
+    except (DivisionZeroError, IndexError) as msg:
+        print("Exception: {}".format(msg), file=sys.stderr
+        return None
