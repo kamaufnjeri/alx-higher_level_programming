8d9bfccc7cf61c1f4bed09a1af12a8047752d466
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-11 16:59:30 +0000

    class to raise errors

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,9 +6,20 @@ class BaseGeometry():
     """Raise Exceptions"""
 
     def area(self):
+        """raise Exception"""
+
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """
+            Checks errors in value
+            Args:
+                name: Name of value
+                value: checked for error
+            Raises:
+                TypeError: type of value if int
+                ValueError: value entered if les than 1
+        """
         if type(value) != int:
             raise TypeError("<name> must be an integer")
         if value <= 0:
