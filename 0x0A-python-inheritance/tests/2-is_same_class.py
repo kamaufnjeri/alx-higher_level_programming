2d7dc6853d999cea98a4fc6a28ad303cfb3e3427
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-11 14:53:14 +0000

    check if an object i of a certain type

---
+++ 0x0A-python-inheritance/tests/2-is_same_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines if obj is exactly the specified class a_class"""
+
+
+def is_same_class(obj, a_class):
+    """Check if obj is class a_class
+    
+    Args:
+        obj: object to check
+        a_class: class to find if obj belongs t0
+    Returs:
+        If obj is of type a_class - True
+        Otherwise - False
+        """
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
