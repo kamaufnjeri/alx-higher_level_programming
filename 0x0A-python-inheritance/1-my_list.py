d5b7c07e9a3860e3ddc7385070ef8d702c0ab442
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-11 13:54:07 +0000

    created a class to print a list in ascending order

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,5 +6,5 @@ class MyList(list):
     """def function defining a list to pe printed sorted"""
 
     def print_sorted(self):
-        """A list to be printed in sorted order""""
+        """A list to be printed in sorted order"""
         print(sorted(self))
