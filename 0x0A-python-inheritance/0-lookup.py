c44dac553e632c98f71b9741b4ab3e74428156b0
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-11 13:47:03 +0000

    function that returns list of objects attributess

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Lookup function to list attributes and methods of an object"""
+
 
 def lookup(obj):
+    """Return a list of attributes and methods of object"""
     return dir(obj)
