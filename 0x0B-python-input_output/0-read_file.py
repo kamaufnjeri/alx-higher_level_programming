9b6e456cc95dcd219333f101d3c38e767cb887a6
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-12 15:37:44 +0000

    read a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Reads into a text file"""
+
+
+def read_file(filename=""):
+    """open file using with"""
+
+    with open(filename, mode='r', encoding='utf-8') as a_file:
+        print(a_file.read())
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+Python Input/ Output opening writing and appending files in python
