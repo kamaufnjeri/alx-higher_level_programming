44ed7128c49265c267562b9890ab1480453e0931
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-12 15:53:43 +0000

    read contents of a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """open file using with"""
 
     with open(filename, mode='r', encoding='utf-8') as a_file:
-        print(a_file.read())
+        print(a_file.read(), end='')
