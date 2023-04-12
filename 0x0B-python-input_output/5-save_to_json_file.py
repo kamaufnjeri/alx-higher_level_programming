f1e54ef0dd5ae614b498ab91a4b27ebf654fed23
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-12 18:53:45 +0000

    add to json file object

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import json
+"""save objects to json"""
+
+
+def save_to_json_file(my_obj, filename):
+    """save opject to file"""
+    with open(filename, 'w', encoding='utf-8') as files:
+        json.dump(my_obj, files)
