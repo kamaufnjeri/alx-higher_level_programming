2f809567255eade87261e6636e15c5abeb6012ba
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-04-13 10:11:56 +0300

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,9 +5,9 @@ class MyInt(int):
     """Override == operator and !="""
 
     def __eq__(self, b):
-
+        """Equal to override be not equal to"""
         return super().__ne__(b)
 
     def __ne__(self, b):
-
+        """Not equal to override by equal to"""
         return super().__eq__(b)
