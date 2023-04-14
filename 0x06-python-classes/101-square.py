2935ba4c06b181093dfe5e637c10d9c5ec0231e4
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-14 20:47:05 +0000

    class to __str__ to print"

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -51,12 +51,12 @@ class Square(object):
             print("")
 
     def __str__(self):
-        """Print representation of squares"""
-        if self.size == 0:
-            return
-        else:
-            str = '\n' * self.__position[1]
-        for i in range(self.__size):
-            str += ' ' * self.position[0]
-            str += '#' * self.__size + '\n'
-        return str[:-1]
+        """Define the print() representation of a Square."""
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
+        return ("")
