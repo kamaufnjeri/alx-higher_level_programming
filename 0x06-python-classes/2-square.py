782dc8b4fb7f0a7417e75be2daeab82089cfacaa
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-14 15:39:21 +0000

    raise type and value errors

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""creating class square"""
+
+
+class Square:
+    """make size a private instance attribute"""
+
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
