55cbc9bcb0fb04bb0c4ba4ff40de754257d11e06
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-14 21:28:58 +0000

    python bytecode

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import math
+"Magic class converting python bytecode to class"""
+
+
+class MagicClass:
+    """Class fo assemble bytecode"""
+
+    def __init__(self, radius=0):
+        self.__radius = 0
+
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be an integer')
+        self.__radius = radius
+
+    def area(self):
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        return 2 * math.pi * self.__radius
