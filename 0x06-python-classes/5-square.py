c2543ec1d625f25b8ce81bd7e51ed6df8c0671c6
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-14 16:14:39 +0000

    print a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""creating class square"""
+
+
+class Square:
+    """make size a private instance attribute"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+
+        if (self.__size) == 0:
+            print()
+
+        else:
+            for i in range(self.__size + 1):
+                for g in range(self.__size + 1):
+                    print("#", end="")
+                print()
