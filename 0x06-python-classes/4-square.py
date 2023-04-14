a02abf8383eb70782065726300df63d1da16346d
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-14 15:58:07 +0000

    getters and setters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""creating class square"""
+
+
+class Square:
+    """make size a private instance attribute"""
+
+    def __init__(self, size):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
