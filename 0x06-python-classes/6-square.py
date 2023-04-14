9f22215443da8b05d417ae7a1514bfdc8c129ccc
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-14 17:40:21 +0000

    check for tuple or if tuple has strings or negative numbers

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,13 +39,12 @@ class Square(object):
     def my_print(self):
         if self.__size == 0:
             print("")
-            return
 
-        for i in range(self.__position[1]:
-                print("")
+        for i in range(self.__position[1]):
+            print("")
         for i in range(self.__size):
-            for j in range(0, self.__position[0])]:
+            for j in range(0, self.__position[0]):
                 print(" ", end="")
-            for k in range(0, self.__size)
+            for k in range(0, self.__size):
                 print("#", end="")
             print("")
