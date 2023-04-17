a4ea0a28737fe96dbd1a97d30a1720fab27b0d83
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-17 18:01:37 +0000

    test files creates

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -12,11 +12,11 @@ class Base:
 
     def __init__(self, id=None):
         """Initialize"""
-        if id is not None:
-            self.id = id
-        else:
+        if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+        else:
+            self.id = id
 
     @staticmethod
     def to_json_string(list_dictionaries):
