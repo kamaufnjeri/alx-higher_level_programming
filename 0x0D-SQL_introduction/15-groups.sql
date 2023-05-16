3a2a506bfd456dfe10440e3402a4b1e4bfb8c03b
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-05-16 20:29:47 +0000

    task 14 and 15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists number of records by score
-SELECT score, COUNT(*) AS 'number' FROM second_table GROUP BY score ORDER BY 'number' DESC; 
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score` ORDER BY number DESC;
