c5b88f94438d26504b73a0551f88cbc0e31e9994
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-05-17 13:01:49 +0000

    all tv shows that are comedy

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,8 @@
+-- all tv shows that are comedy sorted by tittle ascending
+SELECT s.title
+FROM tv_shows s LEFT OUTER JOIN tv_show_genres sg
+ON s.id = sg.show_id
+LEFT OUTER JOIN tv_genres g
+ON g.id = sg.genre_id
+WHERE g.name = 'Comedy'
+ORDER BY s.title;
