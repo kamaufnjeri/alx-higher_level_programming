a0a95a0cc3d5f9b0ceea0796e6194818433d552d
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-05-17 13:31:00 +0000

    not in genre

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,14 @@
+SELECT DISTINCT t.name
+FROM tv_genres t INNER JOIN tv_show_genres tg
+ON t.id = tg.genre_id
+INNER JOIN tv_shows s
+ON s.id = tg.show_id
+WHERE t.name NOT IN (
+	SELECT g.name
+	FROM tv_genres g
+	INNER JOIN tv_show_genres tg
+	ON tg.genre_id = g.id 
+	INNER JOIN tv_shows s
+	ON s.id = tg.show_id
+	WHERE s.title = 'Dexter')
+ORDER BY t.name;
