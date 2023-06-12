0d12ed35a8c87cc9ff9118c87256695ae263863c
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-06-12 18:48:21 +0000

    loops

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
-
+const myArg = process.argv[2];
+if (myArg === undefined) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < Number(myArg); i++) {
+    console.log('C is fun');
+  }
+}
