898d908a52d2931a89c275bfa807ba00d76dbd29
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-06-12 20:25:40 +0000

    second largest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const myArr = process.argv;
+const numArr = myArr.slice(2, myArr.length);
+const sortArr = numArr.sort();
+if (sortArr.length === 0) {
+  console.log(0);
+} else if (sortArr.length === 1) {
+  console.log(0);
+} else {
+  const index = sortArr.length - 2;
+  console.log(sortArr[index]);
+}
