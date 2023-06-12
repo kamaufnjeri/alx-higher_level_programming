a827d9763539a301d2a65336237e449853fe8f01
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-06-12 19:26:37 +0000

    adding

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+function add (a, b) {
+  return a + b;
+}
+const num1 = Number(process.argv[2]);
+const num2 = Number(process.argv[3]);
+const sum = add(num1, num2);
+console.log(sum);
