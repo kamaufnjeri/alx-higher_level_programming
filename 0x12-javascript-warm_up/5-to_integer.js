4be540d120173a58dee081a7f00dee6ab01b8f86
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-06-12 18:32:15 +0000

    Nan

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@ const myArg = process.argv;
 if (typeof (myArg[2]) === 'string') {
   const num = Number(myArg[2]);
   if (num === num) {
-    console.log('My number: ', num);
+    console.log('My number:', num);
   } else {
     console.log('Not a number');
   }
