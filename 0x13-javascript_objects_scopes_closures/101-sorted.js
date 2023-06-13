8125025fc0b49299a6f8f12e9219d0510c5a0694
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-06-13 15:32:34 +0000

    sort dict

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-let newDict = {};
+const newDict = {};
 for (const key in dict) {
   if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [];
+    newDict[dict[key]] = [key];
   } else {
     newDict[dict[key]].push(key);
   }
