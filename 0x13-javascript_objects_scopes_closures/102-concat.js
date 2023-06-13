35ade6f23dfa00a2a2275f8828d07642bc21aae4
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-06-13 16:04:28 +0000

    file concatenate

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const a = fs.readFileSync(process.argv[2], 'utf8');
+const b = fs.readFileSync(process.argv[3], 'utf8');
+const data = a + b;
+fs.writeFileSync(process.argv[4], data);
