f89bc0ef2e874aec36a776f6c1eb6bc19b7ffed0
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-06-16 15:56:54 +0000

    MySQLdb

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""Using MySQLdb"""
-import sys
+"""Usng MySQL"""
 import MySQLdb
-
+import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         database=sys.argv[3])
+                           port=3306,
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           database=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states\
-            WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4]))
-    query = cur.fetchall()
-    for row in query:
-        print(row)
+    cur.execute("""SELECT * FROM states \
+        WHERE name= '{}' ORDER BY id ASC""".format(sys.argv[4]))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1] == sys.argv[4]:
+            print(row)
+
     cur.close()
     db.close()
