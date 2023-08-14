69698d6038c3ab0952459f4d1a8e310d6a98b8df
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-08-14 21:47:56 +0300

    argments

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const count = process.argv[2];
 
-if (count > 0) {
+if (count === 0) {
+  console.log('Missing number of occurrences');
+} else {
   for (let i = 0; i < count; i++) {
     console.log('C is fun');
   }
-} else {
-  console.log('Missing number of occurrences');
 }
