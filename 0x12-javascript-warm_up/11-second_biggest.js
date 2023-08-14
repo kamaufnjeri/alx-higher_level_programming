3ef63be6a08f7a65571a87171a35a930e023ba2f
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-08-14 23:32:24 +0300

    argments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,9 +4,19 @@ const myArray = [];
 for (let i = 0; i < process.argv.length - 2; i++) {
   myArray.push(Number(process.argv[i + 2]));
 }
-const mySortedArray = myArray.sort();
-if (mySortedArray.length <= 1) {
-  console.log(0);
-} else {
-  console.log(mySortedArray[mySortedArray.length - 2]);
+
+function secondBiggest (arr) {
+  const arrSort = arr.sort();
+  if (arrSort.length < 2) {
+    return 0;
+  } else {
+    for (let i = arrSort.length - 1; i >= 0; i--) {
+      if (arrSort[arrSort.length - 1] !== arrSort[i]) {
+        return arrSort[i];
+      }
+    }
+  }
 }
+
+const secondBig = secondBiggest(myArray);
+console.log(secondBig);
