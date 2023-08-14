fd88baee2a9971795134f4ede042a87abb9bf166
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-08-14 22:14:28 +0300

    argments

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
-let myNum = parseInt(process.argv[2]);
+const myNum = parseInt(process.argv[2]);
 
-function factorial(num) {
-	if (isNaN(num) === true || num === 1) {
-		return 1;
-	} else if (num === 0) {
-		return;
-	} else {
-		return num * factorial(num - 1);
-	}
+function factorial (num) {
+  if (isNaN(num) === true || num === 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 }
 
-let myFact = factorial(myNum);
+const myFact = factorial(myNum);
 console.log(myFact);
