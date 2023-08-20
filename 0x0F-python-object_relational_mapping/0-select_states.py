93ced54aba080b107574d1c4bd4973830dfb0ad1
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-08-20 13:17:44 +0300

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-import MySQLdb
+"""List all states from database"""
 import sys
-"Connecting to a daabase"
+import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            port=3306
-            )
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id")
-    rows = cur.fetchall()
-    for row in rows:
-        print("{}".format(row))
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
