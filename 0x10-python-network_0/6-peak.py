ad66ac2d58226471248a02d7bc32cebc39f5b256
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-08-31 17:51:10 +0300

    size in bytes

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,15 +5,14 @@
 def find_peak(list_of_integers):
     """Return a peak in a list of unsorted integers."""
     lis = list_of_integers[:]
-    if len(lis) == 0:
+
+    size = len(lis)
+    if size == 0:
         return None
-    if len(lis) == 1:
+    if size == 1:
         return lis[0]
-    if len(lis) == 2:
-        if lis[1] > lis[0]:
-            return lis[1]
-        else:
-            return lis[0]
+    if size == 2:
+        max(lis)
 
     mid = len(lis) // 2
     middle = lis[mid]
