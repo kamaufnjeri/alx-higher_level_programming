e9cead153045995fc058e5fe73a9961a2d362292
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-08-31 17:14:01 +0300

    size in bytes

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/binn/python3
+"""Module to find a peak"""
+
+def find_peak(list_of_integers):
+    """The function"""
+    if not list_of_integers:
+        return None
+
+    low = 0
+    high = len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
