cd885961be39adec5259e11453fcfe945427614f
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-09-01 16:26:42 +0300

    http request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""request a url"""
+"""REquesthttp"""
 import sys
-from urllib.parse import urlencode
+import urllib.parse
 from urllib.request import urlopen, Request
 
 
@@ -9,10 +9,11 @@ if __name__ == "__main__":
     try:
         url = sys.argv[1]
         value = {"email": sys.argv[2]}
-        data = urlencode(value)
+        data = urllib.parse.urlencode(value)
         data = data.encode('ascii')
         req = Request(url, data)
         with urlopen(req) as response:
-            print(response.read())
+            print(response.read().decode('utf-8'))
     except IndexError:
         pass
+
