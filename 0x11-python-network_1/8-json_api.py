794937c8c871fbd0b91cc2fd510c24c3d1cab78d
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-09-01 17:38:02 +0300

    http request

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Using requests module"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    data = {}
+    if sys.argv[1]:
+        data['q'] =  sys.argv[1]
+    else:
+        data['q'] = ""
+    try:
+        request = requests.post(url, data=data)
+        d_json = request.json()
+
+        if d_json == {}:
+            print("No result")
+        else:
+            print(f"[{d_json.get('id')}] {d_json.get('name')}")
+
+    except ValueError:
+        print("Not a valid JSON")
+~                                   
