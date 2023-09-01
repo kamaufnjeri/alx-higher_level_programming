87bf52f0011d984aec287486f47bd1e2a491758c
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-09-01 15:21:20 +0300

    http request

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-from urllib.request import urlopen
-"""requesting a url"""
+"""request a url"""
+from urllib.request import urlopen, Request
 
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
-    with urlopen(url) as body:
+    req = Request(url)
+    with urlopen(req) as response:
+        body = response.read()
         print("Body response:")
         print(f"\t- type: {type(body)}")
-        print(f"\t- content: {body.read()}")
+        print(f"\t- content: {body}")
         print(f"\t- utf8 content: {body.decode('utf-8')}")
