9b2074a4561d62128a05fc9668bcfd299e2c9c0f
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-09-01 16:47:21 +0300

    http request

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     request = requests.get(url)
     print("Body response:")
-    print(f"\t - type: {type(request)}")
-    print(f"\t - content: {request.text}")
+    print(f"\t- type: {type(request)}")
+    print(f"\t- content: {request.text}")
