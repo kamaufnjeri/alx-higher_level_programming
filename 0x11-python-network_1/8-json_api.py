96c1fb6b011f986f8d5bda9e5abcd874c70005da
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-09-01 17:48:20 +0300

    http request

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""Using requests module"""
+"""JSON search API"""
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    url = 'http://0.0.0.0:5000/search_user'
-    data = {}
-    if sys.argv[1]:
-        data['q'] = sys.argv[1]
-    else:
-        data['q'] = ""
-    try:
-        request = requests.post(url, data=data)
-        d_json = request.json()
+    if len(sys.argv) <= 1:
+        a_dict = {'q': ''}
+    elif len(sys.argv) > 1:
+        a_dict = {'q': sys.argv[1]}
 
-        if d_json == {}:
-            print("No result")
+    try:
+        req = requests.post('http://0.0.0.0:5000/search_user', a_dict)
+        if req.json():
+            print('[{}] {}'.format(req.json().get('id'),
+                                   req.json().get('name')))
         else:
-            print(f"[{d_json.get('id')}] {d_json.get('name')}")
+            print('No result')
 
     except ValueError:
-        print("Not a valid JSOS")    
+        print("Not a valid JSON")
