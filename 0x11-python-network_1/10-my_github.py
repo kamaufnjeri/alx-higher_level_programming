7119fc6035f186dd1c82aa2cfe6227aed18524f4
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-09-01 18:00:19 +0300

    get my id from github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,20 +5,9 @@ import sys
 
 
 if __name__ == "__main__":
-    url = 'http://0.0.0.0:5000/search_user'
-    data = {}
-    if sys.argv[1]:
-        data['q'] = sys.argv[1]
-    else:
-        data['q'] = ""
-    try:
-        request = requests.post(url, data=data)
-        d_json = request.json()
-
-        if d_json == {}:
-            print("No result")
-        else:
-            print(f"[{d_json.get('id')}] {d_json.get('name')}")
-
-    except ValueError:
-        print("Not a valid JSOS")    
+    url = 'https://api.github.com/user'
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+    request = requests.get(url, auth=(username, passwd))
+    my_id = request.json().get('id')
+    print(my_id)
